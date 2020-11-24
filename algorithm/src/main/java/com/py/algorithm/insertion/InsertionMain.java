8a3cd669c2976c831403245dd4d27bbedd147e6d
budingxie <3257632906@qq.com>
2020-11-24 13:45:44 +0800

    插入排序

---
+++ algorithm/src/main/java/com/py/algorithm/insertion/InsertionMain.java
@@ -0,0 +1,34 @@
+package com.py.algorithm.insertion;
+
+import java.util.Arrays;
+
+/**
+ * description
+ *
+ * @author pengyou@xiaomi.com
+ * @version 1.0.0
+ * @date 2020/11/24
+ */
+public class InsertionMain {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 0, 6};
+        System.out.println(Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                // 交换
+                arr[j] = arr[j] ^ arr[j + 1];
+                arr[j + 1] = arr[j] ^ arr[j + 1];
+                arr[j] = arr[j] ^ arr[j + 1];
+//                int tmp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
